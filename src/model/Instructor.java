4264f44a8ac7a99602776345d3ccbd68bdb34973
Brian Chen <brian.chen205@outlook.com>
2024-12-07 15:22:08 -0500

    instructor update

--- src/model/Instructor.java
+++ src/model/Instructor.java
@@ -2,24 +2,36 @@ package model;
 
 import java.io.Serializable;
 
-public class Instructor implements Serializable{
+public class Instructor implements Serializable, Comparable<Instructor>{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
 	private Section[] sectionsTaught;
+	private DateOfEmployment hireDate;
 	private double credits;
 	private Name name;
 	private String id;
 	private Section[] prefSection;
 	private DayOfWeek[] prefDOW;
 	private TimeRange[] prefTime;
+	private double prefCredits;
+	private double curCredits;
 
 	
 	
 	//add other parameters later major/gender/contact info?/etc
 	public Instructor(Name name) {
 		this.name = name;
+	}
+
+
+
+	@Override
+	public int compareTo(Instructor o) {
+		// TODO Auto-generated method stub
+		//
+		return this.hireDate.compareTo(o.hireDate);
 	}
 }
